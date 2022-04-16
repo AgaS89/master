e4c77f540fc5ea7dcd81d949755a852c2b7496bf
Aga <aga.gaga.bb@gmail.com>
2022-04-16 15:34:44 +0200

    commit

--- src/test/java/TestHelper/TestHelperTest.java
+++ src/test/java/TestHelper/TestHelperTest.java
@@ -3,8 +3,9 @@ package TestHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
-import java.util.function.BooleanSupplier;
+
 
 public class TestHelperTest {
 
@@ -14,7 +15,7 @@ public class TestHelperTest {
     @DisplayName("Checking list contains 'Cat' string in")
     public void testGetAnimalsHasValueCat() {
 
-        List<String> animalListCheck =  testHelper.getAnimals();
+        List<String> animalListCheck = testHelper.getAnimals();
         Assertions.assertTrue(animalListCheck.contains("Cat"));
     }
 
@@ -44,7 +45,7 @@ public class TestHelperTest {
 
     @Test
     @DisplayName("Checking list doesn't not contain numbers value in")
-    public void  testGetAnimalsListDoesntContainNumbers() {
+    public void testGetAnimalsListDoesntContainNumbers() {
         List<String> animalList = testHelper.getAnimals();
         String d = "10";
         int a;
@@ -66,69 +67,150 @@ public class TestHelperTest {
 
     @Test
     @DisplayName("Checking is 7 first names in list")
-    public void testGetFirstNamesFMhas7names(){
+    public void testGetFirstNamesFMhas7names() {
         List<String> firstNamesFemaleAndMaleList = testHelper.getFirstNamesFM();
         Assertions.assertTrue(firstNamesFemaleAndMaleList.size() == 7, "List od first names has " + firstNamesFemaleAndMaleList.size() + " records");
     }
 
     @Test
     @DisplayName("Checking list has name Aga on the list")
-    public void testGetFirstNamesFMhasNataliaNames(){
+    public void testGetFirstNamesFMhasNataliaNames() {
         List<String> firstNamesFemaleAndMaleList = testHelper.getFirstNamesFM();
         Assertions.assertTrue(firstNamesFemaleAndMaleList.contains("Natalia"), "Lista doesn't have 'Natalia' first name");
-        }
+    }
 
 
-     @Test
+    @Test
     @DisplayName("Checking list has Integer")
-    public void testGetFirstNamesFMhasInteger(){
-         List<String> firstNamesFemaleAndMaleList = testHelper.getFirstNamesFM();
-         int a;
+    public void testGetFirstNamesFMhasInteger() {
+        List<String> firstNamesFemaleAndMaleList = testHelper.getFirstNamesFM();
+        int a;
 
-         for (int i = 0; i < firstNamesFemaleAndMaleList.size(); i++) {
-             try {
-                 a = Integer.parseInt(firstNamesFemaleAndMaleList.get(i));
-                 System.out.println("Test case failed, has Integer on the list");
-             } catch (Exception e) {
-             }
-         }
-     }
-
-     @Test
-    @DisplayName("Checking list is not empty")
-     public void testGetFirstNamesFMisNotEmpty(){
-         List<String> firstNamesFemaleAndMaleList = testHelper.getFirstNamesFM();
-        Assertions.assertFalse(firstNamesFemaleAndMaleList.isEmpty(), "Test case failed, list is empty");
-     }
+        for (int i = 0; i < firstNamesFemaleAndMaleList.size(); i++) {
+            try {
+                a = Integer.parseInt(firstNamesFemaleAndMaleList.get(i));
+                System.out.println("Test case failed, has Integer on the list");
+            } catch (Exception e) {
+            }
+        }
+    }
 
     @Test
     @DisplayName("Checking list is not empty")
-    public void testGetMaleFirstNamesIsNotEmpty(){
+    public void testGetFirstNamesFMisNotEmpty() {
+        List<String> firstNamesFemaleAndMaleList = testHelper.getFirstNamesFM();
+        Assertions.assertFalse(firstNamesFemaleAndMaleList.isEmpty(), "Test case failed, list is empty");
+    }
+
+    @Test
+    @DisplayName("Checking list is not empty")
+    public void testGetMaleFirstNamesIsNotEmpty() {
         List<String> maleFirstNames = testHelper.getMaleFirstNames();
         Assertions.assertFalse(maleFirstNames.isEmpty(), "Test case failed, list is empty");
     }
 
     @Test
     @DisplayName("Checking list contains 4 names")
-    public void testGetMaleFirstNamesHas4Names(){
+    public void testGetMaleFirstNamesHas4Names() {
         List<String> maleFirstNames = testHelper.getMaleFirstNames();
         Assertions.assertTrue(maleFirstNames.size() == 4, "Test case failed list has " + maleFirstNames.size() + " names");
-            }
+    }
 
-     @Test
+    @Test
     @DisplayName("Checking list contains first name 'Grzegorz'")
-    public void testGetMaleFirstNameContainsGrzegorz(){
+    public void testGetMaleFirstNameContainsGrzegorz() {
         List<String> maleFirstNames = testHelper.getMaleFirstNames();
         Assertions.assertTrue(maleFirstNames.contains("Grzegorz"), "Test case failed, list doesn't complain first name 'Grzegorz'");
-     }
+    }
 
-     @Test
+    @Test
     @DisplayName("Checking list doesn't contain female first names")
-    public void testGetMaleFirstNameDoesntHavefemaleNames(){
-         List<String> maleFirstNames = testHelper.getMaleFirstNames();
-        Assertions.assertFalse(maleFirstNames == maleFirstNames.stream().filter(s -> s.endsWith("a")), "Test case failed. List has female first names");
-        }
-     }
+    public void testGetMaleFirstNameDoesntHavefemaleNames() {
+        List<String> maleFirstNames = testHelper.getMaleFirstNames();
+        Assertions.assertEquals(maleFirstNames.size(), maleFirstNames.stream().filter(s -> !s.endsWith("a")).count(), "Test case failed. List has female first names");
+    }
 
+    @Test
+    @DisplayName("Checking list female first name is not empty")
+    public void testGetFemaleFirstNameIsNotEmpty() {
+        List<String> femaleNames = testHelper.getFemaleFirstName();
+        Assertions.assertFalse(femaleNames.isEmpty(), "Test case failed, list is empty");
+    }
+
+    @Test
+    @DisplayName("Checking list contains 4 records")
+    public void testGetFemaleFirstNameHas4Values() {
+        List<String> femaleNames = testHelper.getFemaleFirstName();
+        Assertions.assertTrue(femaleNames.size() == 4, "Test case failed, list has " + femaleNames.size() + " records in");
+    }
+
+    @Test
+    @DisplayName("Checking female first name list contains 'Alicja'")
+    public void testGetFemaleFirstNamesHasAlicja() {
+        List<String> femaleNames = testHelper.getFemaleFirstName();
+        Assertions.assertTrue(femaleNames.contains("Alicja"), "List doesn't contain Alicja in");
+    }
+
+    @Test
+    @DisplayName("Checking list contains only female names")
+    public void testGetFemaleFirstNamesHasOnlyFemaleNames() {
+        List<String> femaleNames = testHelper.getFemaleFirstName();
+        String femaleName;
+
+        for (int i = 0; i < femaleNames.size(); i++) {
+            femaleName = femaleNames.get(i);
+
+            Assertions.assertTrue((femaleName.endsWith("a")), "Test case failed. " + femaleName + " is not female name");
+        }
+    }
+
+    @Test
+    @DisplayName("Checking value is between min and max")
+    public void testGetRandomNumberBetweenMinMax() {
+
+        int number = testHelper.getRandomNumber(1, 10);
+        Assertions.assertTrue((number != 0), "Test case failed, random number is" + number);
+    }
+
+    @Test
+    @DisplayName("Checking 15 is out of scope")
+    public void testGetRandomNumber15() {
+        int number = testHelper.getRandomNumber(1, 10);
+
+        Assertions.assertFalse(number == 15, "Test case failed, random number is 15");
+    }
+
+    @Test
+    @DisplayName("Checking list has number 7")
+    public void testGetStaticNumberIsOnList() {
+
+        List<Integer> numbers = testHelper.getStaticNumbers();
+
+           for (int i = 0; i < numbers.size(); i++) {
+
+       Assertions.assertTrue(numbers.contains(7));
+        }
+
+    }
+
+    @Test
+    @DisplayName("List has 7 values")
+    public void testGetStaticNumberhas7values() {
+
+        List<Integer> numbers = testHelper.getStaticNumbers();
+        Assertions.assertTrue(numbers.size() == 7, "Test case failed, list has value " + numbers.size());
+
+    }
+
+    @Test
+    @DisplayName("List is not empty")
+    public void testGetStaticNumberIsNotEmpty() {
+
+        List<Integer> numbers = testHelper.getStaticNumbers();
+        Assertions.assertFalse(numbers.isEmpty());
+
+    }
+
+}
 
 
